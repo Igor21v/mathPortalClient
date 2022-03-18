ec7ee996d72fa2f2c82309830aab06bdbc7858d2
igor21v <igor21v@mail.ru>
2022-03-18 16:02:45 +0300

    Формирование списка тем из массива

--- src/сomponents/themesList/ThemesList.jsx
+++ src/сomponents/themesList/ThemesList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux"
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import slide1 from "../../pictures/themes/1.jpg"
 import slide2 from "../../pictures/themes/2.jpg"
@@ -8,76 +8,19 @@ import slide4 from "../../pictures/themes/4.jpg"
 import Theme from './theme/Theme';
 
 const ThemesList = () => {
-/* const themes = useSelector(state => state.themes.themes) */
-const themes = [{name: "Тема1" , discription: "123"}, {name: "Тема2"}]
+    /* const themes = useSelector(state => state.themes.themes) */
+    const themes = [{ name: "Основные законы", discription: "Основные законы арифметики и алгебры", image: "image/1.jpg" },
+    { name: "Теория множеств", image: "image/2.jpg" },
+    { name: "Тема3", discription: "Краткое содержание темы3 ", image: "image/3.jpg" },
+    { name: "Тема4", discription: "Краткое содержание темы4 ", image: "image/4.jpg" },
+    { name: "Тема5", discription: "Краткое содержание темы5 ", image: "image/5.jpg" }]
     return (
         <div>
             <Container style={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
                 <Row className="justify-content-md-center">
-                {themes.map(theme => 
-                    <Theme theme = {theme}/>
-                )}
-                    
-                    <Col md="auto">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={slide1} />
-                            <Card.Body>
-                                <Card.Title>Основные законы</Card.Title>
-                                <Card.Text>
-                                    Основные законы арифметики и алгебры
-                                </Card.Text>
-                                <Button>Перейти к изучению</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="auto">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={slide2} />
-                            <Card.Body>
-                                <Card.Title>Теория множеств</Card.Title>
-                                <Card.Text>
-                                    Основные законы теории множеств
-                                </Card.Text>
-                                <Button>Перейти к изучению</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="auto">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={slide3} />
-                            <Card.Body>
-                                <Card.Title>Тема1</Card.Title>
-                                <Card.Text>
-                                    Краткое содержание темы 1
-                                </Card.Text>
-                                <Button>Перейти к изучению</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="auto">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={slide4} />
-                            <Card.Body>
-                                <Card.Title>Тема1</Card.Title>
-                                <Card.Text>
-                                    Краткое содержание темы 1
-                                </Card.Text>
-                                <Button>Перейти к изучению</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="auto">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={slide4} />
-                            <Card.Body>
-                                <Card.Title>Тема1</Card.Title>
-                                <Card.Text>
-                                    Краткое содержание темы 1
-                                </Card.Text>
-                                <Button>Перейти к изучению</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                    {themes.map(theme =>
+                        <Theme theme={theme} />
+                    )}
                 </Row>
             </Container>
         </div>
--- src/сomponents/themesList/theme/theme.jsx
+++ src/сomponents/themesList/theme/theme.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { Button, Card, Col} from 'react-bootstrap';
-import slide1 from "../../../pictures/themes/1.jpg"
+import { Button, Card, Col } from 'react-bootstrap';
 
-const Theme = ({theme}) => {
+
+const Theme = ({ theme }) => {
     console.log(theme)
     return (
-        
-            <Col md="auto">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={slide1} />
-                            <Card.Body>
-                                <Card.Title>{theme.name}</Card.Title>
-                                <Card.Text>
-                                    Основные законы арифметики и алгебры
-                                </Card.Text>
-                                <Button>Перейти к изучению</Button>
-                            </Card.Body>
-                        </Card>
-            </Col>
-        
+
+        <Col md="auto">
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={theme.image} />
+                <Card.Body>
+                    {console.log(theme.name)}
+                    <Card.Title>{theme.name}</Card.Title>
+                    <Card.Text>{theme.discription}</Card.Text>
+                    <Button>Перейти к изучению</Button>
+                </Card.Body>
+            </Card>
+        </Col>
+
     );
 };
 
