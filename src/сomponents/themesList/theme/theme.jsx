7e8940c4cf3d46f73277d108d4114eaf8339b277
Igor21v <igor21v@mail.ru>
2022-03-20 22:12:20 +0800

    1

--- src/сomponents/themesList/theme/theme.jsx
+++ src/сomponents/themesList/theme/theme.jsx
@@ -4,10 +4,9 @@ import { Button, Card, Col } from 'react-bootstrap';
 
 const Theme = ({ theme }) => {
     return (
-
         <Col md="auto">
             <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={theme.image} />
+                <Card.Img variant="top" src= {"http://localhost:5000/themes/themePicture/" + theme._id + ".jpg"} />
                 <Card.Body>
                     <Card.Title>{theme.name}</Card.Title>
                     <Card.Text>{theme.discription}</Card.Text>
