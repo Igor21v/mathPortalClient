d5052b03e67bbab68ffcc094d6192a4714018b2a
igor21v <igor21v@mail.ru>
2022-03-21 11:56:01 +0300

    2

--- src/сomponents/pages/themes/Themes.jsx
+++ src/сomponents/pages/themes/Themes.jsx
@@ -1,14 +1,30 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import ThemesList from '../../themesList/ThemesList';
 import { getThemes } from '../../../actions/theme';
 import { useSelector, useDispatch } from "react-redux";
+import { Dropdown } from 'react-bootstrap';
+
 
 
 export const Themes = () => {
     const showThemes = "1"
     const dispatch = useDispatch()
-    useEffect(() =>{
+    useEffect(() => {
         dispatch(getThemes(showThemes))
     })
-       return     (<ThemesList/>);
-}
+    return (
+        <div>
+        < Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                Dropdown Button
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown >
+    < ThemesList />
+    </div>
+    )
+       }
