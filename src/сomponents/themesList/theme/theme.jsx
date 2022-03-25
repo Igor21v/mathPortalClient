9eb5a68e3b73cffa323fcc4e07cab828846762c7
igor21v <igor21v@mail.ru>
2022-03-25 08:08:58 +0800

    1

--- src/сomponents/themesList/theme/theme.jsx
+++ src/сomponents/themesList/theme/theme.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
-import {API_URL} from "../../../config";
+import { API_URL } from "../../../config";
 
 
 const Theme = ({ theme }) => {
-    const borderRed = theme.isPublic ? '' : 'danger' 
+    const borderRed = theme.isPublic ? '' : 'danger'
     console.log(borderRed)
-    const picturePath = theme.hasPicture? 
-    (API_URL + "themes/themePicture/" + theme._id + ".jpg"):
-    (API_URL + "themes/themePicture/1.jpg") 
+    const picturePath = theme.hasPicture ?
+        (API_URL + "themes/themePicture/" + theme._id + ".jpg") :
+        (API_URL + "themes/themePicture/1.jpg")
     return (
-        <Col md="auto">
+        <Col md="auto" >
             <Card style={{ width: '16rem' }} className="mb-3" border={borderRed}>
-                <Card.Img variant="top" src= {picturePath} />
+                <Card.Img variant="top" src={picturePath} />
                 <Card.Body>
                     <Card.Title>{theme.name}</Card.Title>
                     <Card.Text>{theme.discription}</Card.Text>
