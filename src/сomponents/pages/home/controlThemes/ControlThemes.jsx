d0fde5d3beac6eb6650b90cb5d5a5311293700a5
Igor21v <igor21v@mail.ru>
2022-03-30 23:50:22 +0800

    111

--- src/сomponents/pages/home/controlThemes/ControlThemes.jsx
+++ src/сomponents/pages/home/controlThemes/ControlThemes.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import { addTheme } from '../../../../actions/theme';
 
 const ControlThemes = () => {
@@ -10,32 +10,34 @@ const ControlThemes = () => {
         addTheme(nameTheme, discription)
         console.log(nameTheme + '  ' + discription)
     }
-    
+
 
     return (
         <>
-        <Form className='border p-3 rounded-3' onSubmit={AddTheme}>
-        <h2 style = {{textAlign: 'center'}}>Создание темы</h2>
-            <Form.Group className="mb-3">
-                <Form.Label>Наименование темы</Form.Label>
-                <Form.Control 
-                type="text" 
-                onChange={(e) => setName(e.target.value)} 
-                placeholder="Введите наименование темы"
-                value = {nameTheme}/>
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Описание темы</Form.Label>
-                <Form.Control 
-                type="text" 
-                onChange={(e) => setDiscription(e.target.value)} 
-                placeholder="Добвавьте описание темы (необязательно)" 
-                value = {discription}/>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Добавить тему
-            </Button>
-        </Form>
+            <Form className='border p-3 rounded-3' onSubmit={AddTheme}>
+                <h2 style={{ textAlign: 'center' }}>Создание темы</h2>
+                <Form.Group className="mb-3">
+                    <Form.Label>Наименование темы</Form.Label>
+                    <Form.Control
+                        type="text"
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Введите наименование темы"
+                        value={nameTheme} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Описание темы</Form.Label>
+                    <Form.Control
+                        type="text"
+                        onChange={(e) => setDiscription(e.target.value)}
+                        placeholder="Добвавьте описание темы (необязательно)"
+                        value={discription} />
+                </Form.Group>
+
+                <Button variant="primary" type="submit" >
+                    Добавить тему
+                </Button>
+                <span className="text-danger ms-3">Ошибка при создании темы!</span>
+            </Form>
         </>
     );
 };
