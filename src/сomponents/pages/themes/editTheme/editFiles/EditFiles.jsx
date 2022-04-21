aee60d1e0517da7da7882eec759f745edf64cbb8
Igor <Igor21v@mail.ru>
2022-04-21 23:48:16 +0800

    Доработка добавления файлов в темы

--- src/сomponents/pages/themes/editTheme/editFiles/EditFiles.jsx
+++ src/сomponents/pages/themes/editTheme/editFiles/EditFiles.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Col, Form } from 'react-bootstrap';
+import icon from '../../../../../assets/img/pdf.svg';
 
 
-const EditFiles = () => {
+const EditFiles = ({theme}) => {
     return (
         <>
             <Form className='border p-3 rounded-3 mb-3'>
                 <Form.Group controlId="Добавить файлы для обучения" className="mb-3">
                     <Form.Label>Добавить файлы для обучения</Form.Label>
-                    <Form.Control type="file" multiple />
+                    <Form.Control type="file" multiple className='mb-3'/>
+                    {theme.files? 
+                        <div className='d-flex'>
+                        {theme.files.map (file => 
+                        <div className=''>
+                            <img src={icon} alt="" />
+                            <span> {file} </span>
+                        </div>
+                        )}
+                        </div>
+                        :
+                        <span> Файлов для обучения пока что нет... </span> }
                 </Form.Group>
             </Form>
         </>
