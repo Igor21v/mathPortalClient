b353b7cefe77c453ae9d37dc9a63d2538b2e14aa
igor21v <igor21v@mail.ru>
2022-05-17 18:24:37 +0800

    7

--- src/сomponents/pages/account/admin/controlUsers/userPage/UserPage.jsx
+++ src/сomponents/pages/account/admin/controlUsers/userPage/UserPage.jsx
@@ -1,24 +1,24 @@
 import React, { useEffect } from 'react';
 import { Card, Form } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const UserPage = () => {
     const param = useParams()
+    const router = useNavigate ()
     const userList = useSelector(state => state.user.userList)
     const user = userList.find(user => user._id === param.id)
     return (
         <Card className='p-3'>
             <h3 style={{ textAlign: 'center' }}>Страничка ученика: {user.surname} {user.name} </h3>
-            {/* <Form> */}
-            <div className='text-decoration-underline text-danger mt-auto ms-3'
-                style={{ cursor: 'pointer', textAlign: 'right' }}>
-                <p onClick={() => console.log('Переход')}>
-                    Изменить данные пользователя </p>
-                <p onClick={() => console.log('Переход2')}>
-                    Удалить пользователя </p>
-            </div>
-            {/* </Form> */}
+            <img style={{ cursor: 'pointer', position: 'absolute', right: '8px' }}
+                onClick={() => router(`/themes/edit/${user._id}`)}
+                src="/edit.svg"
+                width="36"
+                height="36"
+                alt=""
+            />
+
         </Card>
     );
 };
