24533ea98fcdd557f484a6fbed1ea19e9bc33dea
igor21v <igor21v@mail.ru>
2022-05-19 11:28:28 +0800

    3

--- src/сomponents/icons/iconEdit/IconEdit.jsx
+++ src/сomponents/icons/iconEdit/IconEdit.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './iconEdit.css'
 
 const IconEdit = ({props}) => {
     const router = useNavigate()
     console.log(JSON.stringify(props))
     return (
-        <>
-            <img style={{ cursor: 'pointer' }}
+        <div className='iconedit'>
+            <img className='iconedit__icon'
                 onClick={() => router(props.ref)}
                 src="/edit.svg"
                 width="36"
                 height="36"
                 alt=""
                 />
-        </>
+            <div className='iconedit__text'>Редактировать или удалить</div>
+        </div>
     );
 };
 
