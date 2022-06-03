057b624330fdc8731116cfd98144c79b48fd3c3c
igor21v <igor21v@mail.ru>
2022-06-03 16:41:36 +0800

    1

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,13 +11,33 @@ const $authHost = axios.create({
     baseURL: API_URL
 })
 
-const authInterceptor = config => {
+/* const authInterceptor = config => {
     config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
     return config
-}
+} */
 
-$authHost.interceptors.request.use(authInterceptor)
+$authHost.interceptors.request.use(config => {
+    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
+    return config
+})
 
+
+/* $api.interceptors.response.use((config) => {
+    return config;
+},async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
+            localStorage.setItem('token', response.data.accessToken);
+            return $api.request(originalRequest);
+        } catch (e) {
+            console.log('НЕ АВТОРИЗОВАН')
+        }
+    }
+    throw error;
+}) */
 export {
     $host,
     $authHost
