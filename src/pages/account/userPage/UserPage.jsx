a09acdeb052e4dbabc69ae8518c4b7e874e9c165
Igor Bondarenko <igor21v@mail.ru>
2022-07-31 14:00:32 +0800

    removed user's choice of themes

--- src/pages/account/userPage/UserPage.jsx
+++ src/pages/account/userPage/UserPage.jsx
@@ -6,43 +6,10 @@ import Messeges from './messeges/Messeges';
 import UserFiles from './userFiles/UserFiles';
 
 const UserPage = () => {
-
-    const dispatch = useDispatch()
-    const [dropdown, setDropdown] = useState('General')
-    useEffect(() => {
-        dispatch(getListThemes('onlyPublic', '', 'all'))
-    }, [])
-    const themes = useSelector(state => state.themes.listThemes)
-    const dropDownListGen = [
-        { name: 'Общие', eventKey: 'General' },
-    ]
-    const dropDownListPriv = themes.map(theme => {
-        return {
-            name: theme.name,
-            eventKey: theme._id
-        }
-    })
-    let dropDownListAll = dropDownListGen.concat(dropDownListPriv)
-    console.log('lll' + JSON.stringify(dropDownListAll))
-    const activDropdown = dropDownListAll.find(item => item.eventKey === dropdown)
-    console.log('ttt' + JSON.stringify(activDropdown))
-
-
     return (
         <>
-            <div className='d-flex flex-wrap justify-content-between align-items-end ps-3'>
-                <h4 className='m-0'>
-                    {activDropdown.name}
-                </h4>
-                <div>
-                    <DropdownFilter general={dropDownListGen} private={dropDownListPriv} function={setDropdown} toggleText='Выберите тему' activeItem={dropdown} />
-                </div>
-            </div>
-
-            <UserFiles dropdown={dropdown}/>
-
-            <Messeges dropdown={dropdown}/>
-
+            <UserFiles />
+            <Messeges />
         </>
     );
 };
--- src/pages/account/userPage/userFiles/UserFiles.jsx
+++ src/pages/account/userPage/userFiles/UserFiles.jsx
@@ -8,7 +8,7 @@ import { setUserExtend } from '../../../../reducers/userReducer';
 import FileList from '../../../../components/fileList/FileList';
 import ProcState from '../../../../components/procState/ProcState';
 
-const UserFiles = ({dropdown}) => {
+const UserFiles = () => {
     const currentUser = useSelector(state => state.user.currentUser)
     const param = useParams()
     let userId
@@ -21,14 +21,14 @@ const UserFiles = ({dropdown}) => {
     let inputFiles = React.createRef()
     useEffect(() => {
         console.log('user.id' + userId)
-        dispatch(getUserExtend(userId, dropdown))
+        dispatch(getUserExtend(userId, 'General'))
         return () => {
             dispatch(setUserExtend({}))
         }
-    }, [dropdown])
-    function fileUploadHandler(event) {
+    }, [])
+    function fileUploadHandler() {
         const files = [...inputFiles.current.files]
-        files.forEach(file => dispatch(postUserFile(userId, dropdown, file)))
+        files.forEach(file => dispatch(postUserFile(userId, 'General', file)))
         console.log('отправка файла на сервер')
     }
     const userExtend = useSelector(state => state.user.userExtend)
@@ -45,11 +45,11 @@ const UserFiles = ({dropdown}) => {
         <>
             <Card className='p-3 mt-3' >
                 <h4 className='text-center'>Файлы</h4>
-                <FileList files={userExtend.files} userId={userId} folder={dropdown} />
+                <FileList files={userExtend.files} userId={userId} folder={'General'} />
                 <Form className='border p-3 rounded-3 mt-4'>
                     <Form.Group controlId="Add files">
                         <Form.Label>Добавить файлы</Form.Label>
-                        <Form.Control type="file" multiple className='mb-3' ref={inputFiles}/*  onChange={fileUploadHandler} */ />
+                        <Form.Control type="file" multiple className='mb-3' ref={inputFiles} />
                         <Button className='me-2' onClick={fileUploadHandler}>Добавить</Button>
                         <ProcState procState={procState} />
                     </Form.Group>
