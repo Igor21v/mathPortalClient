216e8c8e6d5c2e31064de61dca25dc155504cbac
Igor Bondarenko <igor21v@mail.ru>
2022-08-18 20:28:42 +0800

    1

--- src/components/messeges/message/Message.jsx
+++ src/components/messeges/message/Message.jsx
@@ -4,18 +4,19 @@ import './message.css'
 
 const Message = ({ mess }) => {
     const currentUser = useSelector(state => state.user.currentUser)
-    console.log('cr' + currentUser.id + '  ' + mess.authorId)
     return (
         <div className={`message ${(currentUser.id == mess.authorId) && 'message__own'}`}>
-            <div>
-                <div className='message__text'>
-                    {mess.message}
-                </div>
-                <div className='message__date'>
-                    {mess.date}
-                </div>
+            
+            <div className='message__text' >
+                {mess.message}
             </div>
+            <div className='message__date' >
+                {mess.date}
+            </div>
+            
+            
 
+            
         </div>
     );
 };
